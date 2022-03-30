dfe8f7381252b925132d71e4726649a91db98a3b
tigertherapies <>
2022-03-30 01:43:37 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,25 @@
+puts "Welcome to the Coder Bank, please enter your name"
+name = gets.chomp
+
+puts "hello #{name}, please choose from the option below
+D- Deposit
+W- Withdraw
+B- Show Balance
+"
+
+user_input = gets.chomp.capitalize
+balance = 0
+
+if user_input == "D"
+  puts "how much would you like to deposit?"
+  amount = gets.chomp.to_i
+  balance = balance + amount
+  puts "Thanks, you have successfully deposited $#{amount}"
+elsif user_input == "W"
+  puts "how much would you like to withdraw?"
+  amount = gets.chomp.to_i
+  balance = balance - amount
+  puts "You withdrew $#{amount}, take the cash"
+else
+  puts "your balance is #{balance}"
+end
